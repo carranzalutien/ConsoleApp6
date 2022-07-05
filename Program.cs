021a120d579ec5700709ecf6e967913c5f359692
Alumno <Alumno@CONECTARIGUALDA>
2022-07-05 16:27:47 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace cambiotemperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float celsius;
+            float fahrenheit;
+            Console.WriteLine("elija Celsius ¨C¨ o Fahrenheit ¨F¨");
+            var indicador = Console.ReadLine();
+            if (indicador.ToUpper() == "F" || indicador.ToUpper() == "C") 
+            {
+                if (indicador.ToUpper() == "F")
+                {
+                    Console.Write("ingrese la temperatura en fahrenheit ");
+                    fahrenheit = float.Parse(Console.ReadLine());
+                    celsius = (fahrenheit - 32) * 5 / 9;
+                    Console.WriteLine($"su temperatura es igual a {celsius} ");
+                 }
+                else
+                {
+                    Console.Write("ingrese la temperatura en celsius ");
+                    celsius = float.Parse(Console.ReadLine());
+                    fahrenheit = (1 * 8f * celsius + 32);
+                    Console.WriteLine($"su temperatura es igual a {fahrenheit}");
+                }
+        }  
+        else
+            {
+                Console.WriteLine("algun dato ingresado es erroneo");
+            }
+        }
+    }
+}
